988971eddd4f10df3965eae8f52123ae0bc5ff0e
sabujmahonto <sabujmahonto@gmail.com>
2023-05-12 15:02:58 +0600

    Blance Transfer system done

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 import "core-js/stable";
 
 /////////////////////////////////////////////////////////////
@@ -6,47 +5,47 @@ import "core-js/stable";
 // Data
 /////////////////////////////////////////////////////////////
 const accounts = [
-    {
-      owner: "Sabuj Mahonta",
-      movements: [2500, 500, -750, 1200, 3200, -1500, 500, 1200, -1750, 1800],
-      interestRate: 1.5, // %
-      password: 1234,
-      movementsDates: [
-        "2021-11-18T21:31:17.178Z",
-        "2021-12-23T07:42:02.383Z",
-        "2022-01-28T09:15:04.904Z",
-        "2022-04-01T10:17:24.185Z",
-        "2022-07-08T14:11:59.604Z",
-        "2022-09-10T17:01:17.194Z",
-        "2022-09-12T23:36:17.929Z",
-        "2022-09-15T12:51:31.398Z",
-        "2022-09-19T06:41:26.190Z",
-        "2022-09-21T08:11:36.678Z",
-      ],
-      currency: "USD",
-      locale: "en-US",
-    },
-    {
-      owner: "Tumpa Rani",
-      movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -300, 1500, -1850],
-      interestRate: 1.3, // %
-      password: 5678,
-      movementsDates: [
-        "2021-12-11T21:31:17.671Z",
-        "2021-12-27T07:42:02.184Z",
-        "2022-01-05T09:15:04.805Z",
-        "2022-02-14T10:17:24.687Z",
-        "2022-03-12T14:11:59.203Z",
-        "2022-05-16T17:01:17.392Z",
-        "2022-08-10T23:36:17.522Z",
-        "2022-09-03T12:51:31.491Z",
-        "2022-09-18T06:41:26.394Z",
-        "2022-09-21T08:11:36.276Z",
-      ],
-      currency: "EUR",
-      locale: "en-GB",
-    },
-  ];
+  {
+    owner: "Sabuj Mahonta",
+    movements: [2500, 500, -750, 1200, 3200, -1500, 500, 1200, -1750, 1800],
+    interestRate: 1.5, // %
+    password: 1234,
+    movementsDates: [
+      "2021-11-18T21:31:17.178Z",
+      "2021-12-23T07:42:02.383Z",
+      "2022-01-28T09:15:04.904Z",
+      "2022-04-01T10:17:24.185Z",
+      "2022-07-08T14:11:59.604Z",
+      "2022-09-10T17:01:17.194Z",
+      "2022-09-12T23:36:17.929Z",
+      "2022-09-15T12:51:31.398Z",
+      "2022-09-19T06:41:26.190Z",
+      "2022-09-21T08:11:36.678Z",
+    ],
+    currency: "USD",
+    locale: "en-US",
+  },
+  {
+    owner: "Tumpa Rani",
+    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -300, 1500, -1850],
+    interestRate: 1.3, // %
+    password: 5678,
+    movementsDates: [
+      "2021-12-11T21:31:17.671Z",
+      "2021-12-27T07:42:02.184Z",
+      "2022-01-05T09:15:04.805Z",
+      "2022-02-14T10:17:24.687Z",
+      "2022-03-12T14:11:59.203Z",
+      "2022-05-16T17:01:17.392Z",
+      "2022-08-10T23:36:17.522Z",
+      "2022-09-03T12:51:31.491Z",
+      "2022-09-18T06:41:26.394Z",
+      "2022-09-21T08:11:36.276Z",
+    ],
+    currency: "EUR",
+    locale: "en-GB",
+  },
+];
 
 /////////////////////////////////////////////////////////////
 // Elements
@@ -74,83 +73,87 @@ const inputTransferAmount = document.querySelector(".form-input-amount");
 const inputLoanAmount = document.querySelector(".form-input-loan-amount");
 const inputCloseUsername = document.querySelector(".form-input-username");
 const inputClosePassword = document.querySelector(".form-input-password");
-const inputFill = document.querySelector(".login-input-username")
-const inputFillPass = document.querySelector(".login-input-password")
+const inputFill = document.querySelector(".login-input-username");
+const inputFillPass = document.querySelector(".login-input-password");
 
 /////////////////////////////////////////////////////////////////////////
 //updateUI
 ////////////////////////////////////////////////////////////////////////
-function updateUI(){
-  displayMovements(currentAccount)
-  displaySummary(currentAccount)
-  displayBalance(currentAccount)
+function updateUI(currentAccount) {
+  displayMovements(currentAccount);
+  displaySummary(currentAccount);
+  displayBalance(currentAccount);
 }
 ////////////////////////////////////////////////////////////////////////////
-// Movements 
+// Movements
 ///////////////////////////////////////////////////////////////////////////
 let currentAccount, timer;
 
-function displayMovements(account){
-containerMovements.innerHTML=""
-const moves = account.movements
-moves.forEach((move,i,) =>{
-// console.log(move);
-const type = move > 0 ? "deposit" :"withdrawal";
-const html =`
+function displayMovements(account) {
+  containerMovements.innerHTML = "";
+  const moves = account.movements;
+  moves.forEach((move, i) => {
+    // console.log(move);
+    const type = move > 0 ? "deposit" : "withdrawal";
+    const html = `
         <div class="movements-row">
         <div class="movements-type movements-type-${type}">${i + 1}
         ${type}</div>
         <div class="movements-date">5 days ago</div>
         <div class="movements-value">${move}$</div>
         </div>
-    `
-    containerMovements.insertAdjacentHTML('afterbegin', html)
-})
+    `;
+    containerMovements.insertAdjacentHTML("afterbegin", html);
+  });
 }
-
 
 //////////////////////////////////////////////////////////////////////////////
-// Summary 
+// Summary
 ///////////////////////////////////////////////////////////////////////////////
-function displaySummary(account){
-// Incomes 
-const incomes = account.movements.filter(move => move > 0).reduce((acc, deposit)=> acc + deposit,0)
-labelSumIn.textContent = `${incomes}$`
-// Outcomes
-const outcomes = account.movements.filter(move => move < 0).reduce((acc, outcome)=>acc + outcome,0)
-labelSumOut.textContent = `${Math.abs(outcomes)}$`
+function displaySummary(account) {
+  // Incomes
+  const incomes = account.movements
+    .filter((move) => move > 0)
+    .reduce((acc, deposit) => acc + deposit, 0);
+  labelSumIn.textContent = `${incomes}$`;
+  // Outcomes
+  const outcomes = account.movements
+    .filter((move) => move < 0)
+    .reduce((acc, outcome) => acc + outcome, 0);
+  labelSumOut.textContent = `${Math.abs(outcomes)}$`;
 
-//Interest
-const interest = account.movements.filter(move =>move > 0)
-.map(deposit => deposit * account.interestRate / 100)
-.filter(interest => interest > 1)
-.reduce((acc, interest) => acc + interest,0)
+  //Interest
+  const interest = account.movements
+    .filter((move) => move > 0)
+    .map((deposit) => (deposit * account.interestRate) / 100)
+    .filter((interest) => interest > 1)
+    .reduce((acc, interest) => acc + interest, 0);
 
-labelSumInterest.textContent = `${interest}$`
-
+  labelSumInterest.textContent = `${interest}$`;
 }
-
 
 /////////////////////////////////////////////////////////////////
 // Balance
 ////////////////////////////////////////////////////////////////
 
-function displayBalance(account){
-    account.balance = account.movements.reduce((acc,move) => acc + move,0)
-  labelBalance.textContent = `${account.balance}$`
+function displayBalance(account) {
+  account.balance = account.movements.reduce((acc, move) => acc + move, 0);
+  labelBalance.textContent = `${account.balance}$`;
 }
 
 ////////////////////////////////////////////////////////////////////////
 // create userName
 /////////////////////////////////////////////////////////////////////////
-function displayUserName(accounts){
-  accounts.forEach((account)=>{
-    account.userName = account.owner.toLowerCase().split(" ").map(word => word.at(0)).join("")
-  })
+function displayUserName(accounts) {
+  accounts.forEach((account) => {
+    account.userName = account.owner
+      .toLowerCase()
+      .split(" ")
+      .map((word) => word.at(0))
+      .join("");
+  });
 }
-displayUserName(accounts)
-
-
+displayUserName(accounts);
 
 ////////////////////////////////////////////////////////////////////////////
 // Login
@@ -165,34 +168,68 @@ btnLogin.addEventListener("click", (e) => {
 
   if (currentAccount?.password === +inputLoginPassword.value) {
     // Display UI and welcome message
-    labelWelcome.textContent = `Welcome back, ${
-      currentAccount.owner.split(" ").at(0)
-    }`;
+    labelWelcome.textContent = `Welcome back, ${currentAccount.owner
+      .split(" ")
+      .at(0)}`;
     labelWelcome.style.color = "#444";
     containerApp.style.opacity = 1;
+    containerApp.style.visibility = "visible";
     btnLogout.style.opacity = 1;
     btnLogout.style.visibility = "visible";
     btnLogin.style.opacity = 0;
     inputFill.style.opacity = 0;
-    inputFill.style.visibility ="hidden"
+    inputFill.style.visibility = "hidden";
     inputFillPass.style.opacity = 0;
-    inputFillPass.style.visibility ="hidden"
-
-    
+    inputFillPass.style.visibility = "hidden";
 
     // Update UI
     updateUI(currentAccount);
-   
   } else {
-     // Hide UI and display warning message
-   labelWelcome.textContent = "Incorrect user or password!";
-   labelWelcome.style.color = "#f3442a";
-   containerApp.style.opacity = 1;
-   
+    // Hide UI and display warning message
+    labelWelcome.textContent = "Incorrect user or password!";
+    labelWelcome.style.color = "#f3442a";
+    containerApp.style.opacity = 1;
+    containerApp.style.visibility = "visible";
   }
-  
+
   // Clear input fields
   inputLoginUsername.value = inputLoginPassword.value = "";
   inputLoginPassword.blur();
-  
+});
+
+/////////////////////////////////////////////////////////////////
+// Transfer
+////////////////////////////////////////////////////////////////
+
+btnTransfer.addEventListener("click", (e) => {
+  e.preventDefault();
+  const receiverAccount = accounts.find(
+    (account) => account.userName === inputTransferTo.value
+  );
+
+  const amount = inputTransferAmount.value;
+
+  if (
+    amount > 0 &&
+    amount <= currentAccount.balance &&
+    currentAccount.userName !== receiverAccount.userName &&
+    receiverAccount
+  ) {
+    // Transfer Money 
+    currentAccount.movements.push(-amount)
+    receiverAccount.movements.push(amount)
+    // Update U
+    updateUI(currentAccount)
+
+    //Show Massage
+    labelWelcome.textContent = "Transfer Successful"
+
+  }else{
+    labelWelcome.textContent = "transfer Fail !"
+    labelWelcome.style.color = "#f3442a";
+  }
+
+  // clear fields
+   inputTransferTo.value = inputTransferAmount.value ="";
+  inputTransferAmount.blur();
 });
